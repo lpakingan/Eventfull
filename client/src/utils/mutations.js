a39c306d00873a8954935ff7d5d06856b257ad30
lpakingan <lpak98@gmail.com>
2023-09-12 14:37:00 -0700

    implement ability to save events from API

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,18 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const ADD_USEREVENT = gql`
+  mutation AddUserEvent($user: String!, $eventData: EventInput!) {
+    addUserEvent(user: $user, eventData: $eventData) {
+      _id
+      user {
+        _id
+      }
+      event {
+        _id
+      }
+      date
+      status
+      preference
+    }
+  }
+`;
