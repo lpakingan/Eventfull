df038e259a5b38207b9fc8173f55dead5eec4c86
lpakingan <lpak98@gmail.com>
2023-09-12 11:27:52 -0700

    add EventSearch page for searching events from API, implement back-end for API search

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,65 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Explore from './components/Explore';
-import Social from './components/Social';
-import Profile from './components/Profile';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './components/styles/theme';
-import GlobalStyles from './components/styles/Global';
-
-
-
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Explore from "./components/Explore";
+import Social from "./components/Social";
+import Profile from "./components/Profile";
+import Search from "./pages/EventSearch";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/styles/theme";
+import GlobalStyles from "./components/styles/Global";
 
 const httpLink = createHttpLink({
-    uri: '/graphql',
-  });
+  uri: "/graphql",
+});
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('id_token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
-  function App() {
-    return (
-      <ApolloProvider client={client}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <BrowserRouter>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Explore" element={<Explore />} />
-              <Route path="/Social" element={<Social />} />
-              <Route path="/Profile" element={<Profile />} />
-            </Routes>
-            <Footer />
-          </BrowserRouter>
-        </ThemeProvider>
-      </ApolloProvider>
-    );
-  }
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Explore" element={<Explore />} />
+            <Route path="/Social" element={<Social />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Search" element={<Search />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ApolloProvider>
+  );
+}
 
-  export default App;
+export default App;
