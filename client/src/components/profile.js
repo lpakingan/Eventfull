65531db262b2d62ab57ae77d84f23b13827e8e39
Joshua Lam <joshualam071522@gmail.com>
2023-09-12 20:52:40 -0700

    added initial styling for profile page, added event list component

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,9 +1,29 @@
-import { StyledProfile } from "./styles/profile.styled"
+import { StyledProfile, StyledProfileIcon, StyledProfileInfo, StyledProfileEventContainer } from "./styles/profile.styled"
+import profileIcon from "../assets/profile-1.png";
+import { useQuery } from "@apollo/client"
+// change to query user or query me to populate user's events
+import { QUERY_EVENTS } from "../utils/queries";
+import EventList from "./EventList";
 
 export default function Profile() {
+    const { data, loading } = useQuery(QUERY_EVENTS);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    const eventData = data?.events || [];
+
     return (
         <StyledProfile>
-            <h1>Your Profile</h1>
+            <StyledProfileInfo>
+                <StyledProfileIcon src={profileIcon} alt="profile Icon"/>
+                    <h3>Username</h3>
+                    <h3>Description: Welcome to my profile!</h3>
+            </StyledProfileInfo>
+            <StyledProfileEventContainer>
+                <EventList events={eventData} />
+            </StyledProfileEventContainer>
         </StyledProfile>
     )
 }
