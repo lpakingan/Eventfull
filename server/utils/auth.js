a6f3d01f80f73e0452115af172ed93ec261996d5
lpakingan <lpak98@gmail.com>
2023-09-14 19:06:29 -0700

    update auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,13 +6,9 @@ const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    if (!req.headers?.authorization) {
-      return next();
-    }
-    // allows token to be sent via  req.query or headers
-    let token = req && req.headers ? req.headers.authorization : null;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
