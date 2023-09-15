792f513a243541f404af084df4426e620b6540e0
giomendes <gioq89@gmail.com>
2023-09-14 17:57:09 -0700

    fixed token creation for login and signup

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,13 @@ const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function ({ req }) {
+  authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    if (!req.headers?.authorization) {
+      return next();
+    }
+    // allows token to be sent via  req.query or headers
+    let token = req && req.headers ? req.headers.authorization : null;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
