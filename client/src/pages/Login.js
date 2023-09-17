7addfb7b0f1b7e3728aa058e7ad46c80cc6d2873
Joshua Lam <joshualam071522@gmail.com>
2023-09-17 00:27:56 -0700

    removed text-decoration for links on login and signup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -88,7 +88,7 @@ const Login = () => {
           <div className="signup-here">
             Don't Have an account?
             <a href="/signup">
-              <span style={{padding: "10px 0"}}>Create Account</span>
+              <span style={{padding: "10px 0"}}>Sign up</span>
             </a>
           </div>
           <div className="submit-container">
